b350ed29e4978a80c9fe60256610f7ab15e7472f
xyzuan <xyzuannihboss@gmail.com>
2022-12-09 14:34:47 +0700

    main: Responsive initial for Mobile

--- js/main.js
+++ js/main.js
@@ -1,8 +1,28 @@
-var navlink = document.querySelectorAll(".navlink");
+const navItems = document.querySelectorAll(".nav-items");
+const navToggle = document.getElementById("nav-toggle");
+const navMenu = document.getElementById("nav-menu");
+const content = document.querySelectorAll("section");
 
 function linkAction() {
-  navlink.forEach((m) => m.classList.remove("active"));
+  navItems.forEach((m) => m.classList.remove("active"));
   this.classList.add("active");
 }
 
-navlink.forEach((m) => m.addEventListener("click", linkAction));
+navItems.forEach((m) => m.addEventListener("click", linkAction));
+
+const showMenu = () => {
+  navMenu.classList.toggle("show-menu");
+  content.forEach((section) => section.classList.toggle("main-blur"));
+};
+
+navToggle.addEventListener("click", showMenu);
+
+const closeMenu = () => {
+  navMenu.classList.remove("show-menu");
+  content.forEach((section) => section.classList.remove("main-blur"));
+};
+
+content.forEach((section) => section.addEventListener("click", closeMenu));
+
+const navLink = document.querySelectorAll(".nav-link");
+navLink.forEach((link) => link.addEventListener("click", closeMenu));
